9758cfe1ad52b13fe2c470fc6f1b79ae3dd91bc7
fabio silva <fbo_vianna@hotmail.com>
2023-07-03 10:27:44 -0300

    nova att 03-07

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.css"
 import Image from "next/image";
-import Card from "../components/Card";
 
 /*--------------------------------------------------------------------------------*/
 
-export async function getStaticProps() {
+/*export async function getStaticProps() {
   const maxPokemons = 251;
   const api = "https://pokeapi.co/api/v2/pokemon";
   const response = await fetch(`${api}/?limit=${maxPokemons}`);
@@ -17,7 +16,7 @@ export async function getStaticProps() {
   return {
     props: { pokemons },
   };
-}
+}*/
 
 const Home = ({ pokemons }) => {
   return (
@@ -27,18 +26,17 @@ const Home = ({ pokemons }) => {
           Poke<span></span>Next
         </h1>
         <Image
-          href="/assets/pokeball.png"
-          widht="50"
-          heigth="50"
+          src="/assets/pokeball.png"
+          width="50"
+          height="50"
           alt="Imagem de uma Pokebola"
         />
+        </div>
 
         <div className={styles.pokemonContainer}>
-          {pokemons.map((pokemon) => {
-            <Card key={pokemon.id} pokemon={pokemon} />;
-          })}
+          <p>testando</p>
         </div>
-      </div>
+      
     </>
   );
 };

5b3640ec296bed7c3e2822bbf044273f6c47fa8b
Fábio Vianna <fbo_vianna@hotmail.com>
2023-07-08 17:39:14 -0300

    commit

--- src/pages/pokemon/[pokemonId].js
+++ src/pages/pokemon/[pokemonId].js
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import styles from "../../styles/PokemonId.module.css";
 
 export const getStaticPaths = async () => {
   const maxPokemons = 151;
@@ -29,10 +31,37 @@ export const getStaticProps = async (context) => {
 };
 
 const Pokemon = ({ pokemon }) => {
+  const imageUrl = pokemon.sprites?.front_default;
+
   return (
-    <>
-      <p>{pokemon.name}</p>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{pokemon.name}</h1>
+      <Image
+        src={imageUrl}
+        width="200"
+        height="220"
+        alt={`imagem do pokemon ${pokemon.name}`}
+      />
+      <div className={styles.DescriptionContainer}>
+        <h3 className={styles.subtitle}>Numero:</h3>
+        <p>{`# ${pokemon.id}`}</p>
+
+        <div className={styles.typeContainer}>
+          <h3 className={styles.subtitle}>Tipo:</h3>
+          <div className={styles.spanContainer}>
+            {pokemon.types.map((item, index) => (
+              <span key={index}>{item.type.name}</span>
+            ))}
+          </div>
+        </div>
+        <div className={styles.info}>
+          <h3 className={styles.subtitle}>Altura:</h3>
+          <p>{`${pokemon.height * 10}`}cm</p>
+          <h3 className={styles.subtitle}>Peso:</h3>
+          <p>{`${pokemon.weight / 10}`}kg</p>
+        </div>
+      </div>
+    </div>
   );
 };
 

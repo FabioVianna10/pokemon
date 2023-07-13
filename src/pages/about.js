8433097a4fc9aa1cbc0a93643800fe1fceb75e9c
fabio silva <fbo_vianna@hotmail.com>
2023-07-13 10:53:31 -0300

    editando a pagina about

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,10 +7,17 @@ const about = () => {
       <div className={styles.container}>
         <h1 className={styles.text}>Sobre o projeto</h1>
         <p className={styles.paragraph}>
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          Este projeto foi desenvolvido com objetivo de colocar em pratica
+          alguns conceitos de desenvolvimento como componentização,
+          componentização por rotas, consumo de Apis, estilização em módulos.
         </p>
+        <h3>Tecnologias Utilizadas</h3>
+        <ul>
+          <li>NextjS</li>
+          <li>React</li>
+          <li>Javascript</li>
+          <li>Module CSS</li>
+        </ul>
         <Image
           src="/assets/charizard.png"
           width="400"
